4ca82ac6acfbb7211ed6380967de634c22307ace
jdelucca <76178990+jdelucca@users.noreply.github.com>
2023-05-04 13:25:54 -0500

    flowified database (mutable state flow)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -4,24 +4,27 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 private const val TAG = "CrimeListViewModel"
-private val crimeRepository = CrimeRepository.get()
+
 
 class CrimeListViewModel: ViewModel() {
-    val crimes = mutableListOf<Crime>()
-    
+    private val crimeRepository = CrimeRepository.get()
+    private val _crimes: MutableStateFlow<List<Crime>> = MutableStateFlow(emptyList())
+    val crimes: StateFlow<List<Crime>>
+        get() = _crimes.asStateFlow()
+
     init {
-        Log.d(TAG, "init starting: ")
         viewModelScope.launch {
-            Log.d(TAG, "coro launched: ")
-            crimes +=loadCrimes()
-            Log.d(TAG, "loading crimes finished: ")
+            crimeRepository.getCrimes().collect{
+                _crimes.value = it
+            }
         }
-    }
-    suspend fun loadCrimes(): List<Crime> {
-        return crimeRepository.getCrimes()
     }
 }
