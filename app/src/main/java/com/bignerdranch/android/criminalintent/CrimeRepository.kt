da1ef8932ad09c01590024b3c1cdb53e800a3988
jdelucca <76178990+jdelucca@users.noreply.github.com>
2023-05-17 18:12:06 -0500

    takes changes from user input and updates crime in database

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -3,12 +3,16 @@ package com.bignerdranch.android.criminalintent
 import android.content.Context
 import androidx.room.Room
 import com.bignerdranch.android.criminalintent.database.CrimeDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import java.util.UUID
 
 private const val DATABASE_NAME ="crime-database"
 
-class CrimeRepository private constructor(context: Context) {
+class CrimeRepository private constructor(context: Context,
+                                          private val coroutineScope: CoroutineScope = GlobalScope) {
     private val database: CrimeDatabase = Room.databaseBuilder(
         context.applicationContext,
         CrimeDatabase::class.java,
@@ -19,6 +23,13 @@ class CrimeRepository private constructor(context: Context) {
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
     suspend fun getCrime(id: UUID): Crime = database.crimeDao().getCrime(id)
+
+    fun updateCrime(crime: Crime) {
+        coroutineScope.launch {
+            database.crimeDao().updateCrime(crime)
+        }
+
+    }
 
     companion object{
         private var INSTANCE: CrimeRepository? = null
